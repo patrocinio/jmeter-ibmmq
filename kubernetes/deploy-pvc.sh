7638e846edb2954ce90ee30c23e1744e9e71900e
Eduardo Patrocinio <epatro@gmail.com>
2021-09-23 11:54:39 -0400

    Create a producer and consumer per thread

--- kubernetes/deploy-pvc.sh
+++ kubernetes/deploy-pvc.sh
@@ -1,4 +1,4 @@
 . ./.envrc
 sed s/PVC_NAME/$PVC_NAME/g < jmeter-pvc.yaml.template > jmeter-pvc.yaml
-#oc delete -f jmeter-pvc.yaml --force
-#oc apply -f jmeter-pvc.yaml
+oc delete -f jmeter-pvc.yaml --force
+oc apply -f jmeter-pvc.yaml
