4d39c31511b47debc72ab9ff63d3987769502004
Eduardo Patrocinio <epatro@gmail.com>
2021-09-28 07:39:13 -0400

    Increase PV size

--- kubernetes/download_results.sh
+++ kubernetes/download_results.sh
@@ -1,4 +1,7 @@
 ./deploy-storage-pod.sh
 
 mkdir -p report
+
+echo Downloading report
 oc cp storage-pod:/opt/apache-jmeter-5.4/tests/ibmmq/report/ report/
+
--- kubernetes/prepare-storage.sh
+++ kubernetes/prepare-storage.sh
@@ -1,5 +1,8 @@
 ./deploy-storage-pod.sh
 
+echo Removing old files
+oc exec storage-pod -- rm -rf /opt/apache-jmeter-5.4/tests/ibmmq/*
+
 echo Copying files
 oc cp ../tests/ibmmq/MQ.jmx storage-pod:/opt/apache-jmeter-5.4/tests/ibmmq
 oc cp ../tests/ibmmq/clientkey.jks storage-pod:/opt/apache-jmeter-5.4/tests/ibmmq
